973f02f9d1b41b45c79488f3207ae6c534439ee7
flamenca91 <81594900+flamenca91@users.noreply.github.com>
2022-12-09 22:00:48 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -19,18 +19,6 @@ root['background'] = '#afeae6'
 
 # ******************************** Test File Search *********************************
 
-'''
-def GetDocDict(filename):
-    global docFile
-    dodFile = {}
-    f = open(docFile)
-    #f = open("DocTagsList.txt")
-    for line in f:
-        line = line.split()
-        docFile[line[0]] = line[1]
-    return docFile
-'''
-
 def openFile():
     global filename
     filename = filedialog.askopenfilename(parent=root, title='Choose a file')
@@ -43,7 +31,7 @@ def openDirectory():
     print(foldername)
 
 filePath = StringVar(None)
-filepath = Entry(root, borderwidth=5, textvariable=filePath)
+filepath = Entry(root, borderwidth=5, textvariable=filePath)    # Creates a textbox for the tags/Documents file path
 filepath.grid(column=0,row=0,sticky='EW')
 filepath.update()
 filepath.focus_set()
@@ -55,37 +43,20 @@ button.pack(padx = 1, pady = 1,anchor='ne')
 button.place( x = 650, y = 25)
 
 dirPath = StringVar(None)
-dirpath = Entry(root, borderwidth=5, textvariable=dirPath)
+dirpath = Entry(root, borderwidth=5, textvariable=dirPath)   # Creates a textbox for the documents directory
 dirpath.grid(column=0,row=0,sticky='EW')
 dirpath.update()
 #dirpath.focus_set()
 dirpath.pack(padx = 20, pady = 20,anchor='n')
 dirpath.place(y = 100, x = 100, width = 525, height = 25)
 
-button = Button(root, text='Select docs directory',command = openDirectory)
+button = Button(root, text='Select docs directory',command = openDirectory) # Creates a button to run all the code
 button.pack(padx = 1, pady = 1,anchor='ne')
 button.place( x = 650, y = 105)
 
-#command=lambda: [fun1(), fun2()])
+# When button is clicked, it runs the code in the docExtract.py file which contains the GetRelationaList function
 tagFileBtn = Button(root, text='Extract Data', padx=40, command = GetRelationalList ,pady=10, bg = 'white')
 tagFileBtn.place(x=100,y=160)
 
 root.mainloop()
 
-'''
-leadTags = GetLeadingTags()[0]
-print("There are", len(leadTags), "leading tags")
-print(leadTags[106])
-reducedLeadingTags = GetRelLeadTags()
-print("There are", len(reducedLeadingTags[0]), "leading tags excluding RISK and URS")
-trailingTagsList = GetTrailingTags()
-#print(trailingTagsList[66])
-print("There are", len(trailingTagsList), "trailing tags")
-DescriptionTagsList = GetLeadingTags()[1]
-print("There are", len(DescriptionTagsList), "tags with descriptions")
-uniqueTagsList = GetUniqueTrailingTags()
-print("There are", len(uniqueTagsList), "unique trailing tags")
-verifTags = GetVerifiedUniqueTrailingTags()
-print("There are", len(verifTags[0]), "tags without PASS/FAIL data or Orphans")
-print("There are", len(verifTags[1]), "orphan tags")
-'''
